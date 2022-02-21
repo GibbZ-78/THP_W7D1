3c04431fde36b34212b97767bccd9f75fc071df4
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-02-21 15:00:05 +0100

    push de sauvegarde après premier envois de mail concluants

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,12 @@
 class UserMailer < ApplicationMailer
   
   default from: 'jb.vidal@gmail.com'
+  default subject: "[Default subject] Your call to the 'mail' function may be missing a 'subject' argument"
  
   def welcome_email(my_user)
     @user = my_user 
     @url  = 'http://monsite.fr/login' 
-    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+    mail(to: @user.email, subject: "Bienvenue chez nous !") 
   end
 
 end
